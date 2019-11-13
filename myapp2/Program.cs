6b9631dee17a5c39d323fe34187f0be43b663cf6
aleksandra sagitova <aleksandrasagitova@aleksandras-MacBook-Pro.local>
2019-11-13 21:32:55 +0300

    while

---
+++ myapp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace myapp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            while (true)
+            {
+                count*=2;
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write("next? ");
+               
+                }
+                     Console.WriteLine();
+                string answer = Console.ReadLine();
+                if (answer == "Q")
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
